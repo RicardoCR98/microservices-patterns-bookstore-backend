f6394f10549303d7aa2494a6cad84880b6facab8
RicardoCR98 <gary.campana@epn.edu.ec>
2025-01-18 14:02:25 -0500

    fix: Security leak

--- microservice-notifications/src/main/java/com/microservice/notifications/MicroserviceNotificationsApplication.java
+++ microservice-notifications/src/main/java/com/microservice/notifications/MicroserviceNotificationsApplication.java
@@ -1,7 +1,6 @@
 package com.microservice.notifications;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- microservice-payments/src/main/java/com/microservice/payments/MicroservicePaymentsApplication.java
+++ microservice-payments/src/main/java/com/microservice/payments/MicroservicePaymentsApplication.java
@@ -4,8 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
 import java.util.Objects;
 
