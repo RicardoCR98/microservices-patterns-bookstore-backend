f13b4c8eaa3720187c6abcb8dda0b3e84e0592f0
RicardoCR98 <gary.campana@epn.edu.ec>
2025-01-23 11:21:00 -0500

    chore: gitignore update and some changes

--- microservice-orders/src/main/java/com/microservice/orders/config/RabbitMQConfig.java
+++ microservice-orders/src/main/java/com/microservice/orders/config/RabbitMQConfig.java
@@ -3,7 +3,6 @@ package com.microservice.orders.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
